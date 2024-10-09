e12a75d5ea7ec0efb8ff7d5c687da03cb392a06d
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2024-10-09 17:28:06 +0200

    mobile menu work in progress

--- js/top-nav.js
+++ js/top-nav.js
@@ -1,6 +1,8 @@
-const buttons = document.querySelectorAll('.top-nav__button');
+/* Desktop navigation */
 
-buttons.forEach((item) => {
+let buttons = document.querySelectorAll('.top-nav__button');
+
+buttons.forEach(item => {
   item.addEventListener('click', () => {
     item.classList.add('top-nav__button--open');
   });
@@ -9,9 +11,26 @@ buttons.forEach((item) => {
 // close the popup when clicking outside
 document.addEventListener('click', (event) => {
   const path = event.composedPath();
-  buttons.forEach((item) => {
+  buttons.forEach(item => {
     if (!path.includes(item)) {
       item.classList.remove('top-nav__button--open');
     }
   });
-})
+});
+
+
+/* Mobile navigation */
+
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('hamburger--open');
+  document.querySelector('.top-nav-mobile').classList.toggle('top-nav-mobile--open');
+});
+
+function openSlideIn(id) {
+  document.getElementById(id).classList.add('top-nav-mobile__slidein--open');
+}
+function closeSlideIn(id) {
+  document.getElementById(id).classList.remove('top-nav-mobile__slidein--open');
+}
